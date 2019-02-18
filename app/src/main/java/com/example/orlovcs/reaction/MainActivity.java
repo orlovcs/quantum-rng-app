29b91e0e347a2197d6280f867d94cae9e3bbb3fe
Nikita <thetax45@gmail.com>
2019-02-18 16:22:08 -0500

    call to api success and async, data extraction working for array recieved

--- app/src/main/java/com/example/orlovcs/reaction/MainActivity.java
+++ app/src/main/java/com/example/orlovcs/reaction/MainActivity.java
@@ -1,22 +1,34 @@
 package com.example.orlovcs.reaction;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    TextView debug;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +55,16 @@ public class MainActivity extends AppCompatActivity
                 firstLottoNum.setText(String.valueOf(1));
             }
         });
+
+        debug = (TextView) findViewById(R.id.textView2);
+        new RetrieveAPI().execute();
+
+        //cant decode it here since its async
+
+
+
+
+
     }
 
     @Override
@@ -101,4 +123,67 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
+    class RetrieveAPI extends AsyncTask<Void, Void, String> {
+
+        private Exception exception;
+
+        protected void onPreExecute() {
+            debug.setText("");
+        }
+
+        protected String doInBackground(Void... urls) {
+
+            try {
+                String API_URL = "https://qrng.anu.edu.au/API/jsonI.php?length=6&type=uint8";
+                URL url = new URL(API_URL);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+                finally{
+                    urlConnection.disconnect();
+                }
+            }
+            catch(Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+        protected void onPostExecute(String response) {
+            if(response == null) {
+                response = "ERROR";
+            }
+
+            Log.i("INFO", response);
+            debug.setText(response);
+
+            // Convert String to json object
+            JSONObject json = null;
+            try {
+
+                json = new JSONObject(response);
+                JSONArray str_value = json.getJSONArray("data"); //<< get value here
+
+                debug.setText(String.valueOf(str_value.length()));
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+
+
+
 }
