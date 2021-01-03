3eb6dbd8589cbdcc18df4b5ee93caaaa4053b5ea
orlovcs <thetax45@gmail.com>
2021-01-03 16:46:28 -0500

    fix for duplicate in original generated number array, list will make sure to not add already included integers

--- app/src/main/java/com/crimsonlabs/orlovcs/reaction/GenerationHelper.java
+++ app/src/main/java/com/crimsonlabs/orlovcs/reaction/GenerationHelper.java
@@ -8,22 +8,40 @@ import java.util.Random;
 public class GenerationHelper {
 
     ArrayList<Integer> manualGeneration() {
-        ArrayList nums = new ArrayList<>();
+        ArrayList nums = new ArrayList<Integer>();
         Random rand = new Random();
         for (int i = 0; i < 40; i++) {
+            //locally generate new number within the API bound
             int n = rand.nextInt(65535); //ANU Bound
-            nums.add(n);
+            if (!nums.contains(n)){
+                nums.add(n);
+            }else{
+                //if generated number is present, generate until a unique new one is found
+                while(nums.contains(n)) {
+                    n = rand.nextInt(65535); //ANU Bound
+                }
+                nums.add(n);
+            }
         }
         return nums;
     }
 
     ArrayList<Integer> processData(JSONArray data_array) {
-        ArrayList nums = new ArrayList<>();
+        ArrayList nums = new ArrayList<Integer>();
+        Random rand = new Random();
         try {
             if (data_array != null) {
                 for (int i = 0; i < data_array.length(); i++) {
-                    int data = data_array.getInt(i);
-                    nums.add(data);
+                    int n = data_array.getInt(i);
+                    if (!nums.contains(n)){
+                        nums.add(n);
+                    }else{
+                        //if generated number is present, generate until a unique new one is found
+                        while(nums.contains(n)) {
+                            n = rand.nextInt(65535); //ANU Bound
+                        }
+                        nums.add(n);
+                    }
                 }
             }
 
